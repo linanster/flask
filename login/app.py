b9573c53d57da37db16b386cfceab581264d8278
linan <believelinan@aliyun.com>
2020-03-07 12:19:28 +0800

    login: add info to seesion

--- login/app.py
+++ login/app.py
@@ -6,13 +6,20 @@ import functools
 
 app=Flask(__name__,template_folder='.',static_url_path='/xxxxxx')
 app.secret_key = "sdfasdfasdf3fsdf"
-authorized = [('nan', '123'), ('zhe', '456')]
+authorized = [('nan', '123', 'Cloud Development Engineer'), ('zhe', '456', 'Product Manager')]
+
+def verify(challenge, authorized):
+    for item in authorized:
+        if item[0] == challenge[0] and item[1] == challenge[1]:
+            return item[2]
+    return None
+    
 
 
 def wapper(func):
     @functools.wraps(func)
     def inner(*args,**kwargs):
-        if not session.get('user_info') and request.path != "/login":
+        if not session.get('user_name') and request.path != "/login":
             return redirect('/login')
         return func(*args,**kwargs)
     return inner
@@ -27,10 +34,12 @@ def login():
         user = request.form.get('user')
         pwd = request.form.get('pwd')
         # if user =='zxc' and pwd == '123':
-        auth = (user, pwd)
-        if auth in authorized:
-            session['user_info'] = user
-            # print(session['user_info'])
+        challenge = (user, pwd)
+        info = verify(challenge, authorized)
+        if info != None:
+            session['user_name'] = user
+            session['user_info'] = info
+            # print(session['user_name'])
             return redirect('/index')
         else:
             return render_template('login.html',msg='Failed!')
@@ -42,17 +51,18 @@ def login():
 @app.route('/index',methods=['GET'])
 @wapper
 def index():
-    user = session['user_info']
-    return  render_template('/index.html', user=user)
+    user = session['user_name']
+    info = session['user_info']
+    return  render_template('/index.html', user=user, info=info)
     
 
 @app.route('/logout', methods=['GET'])
 @wapper
 def logout():
-    session.pop('user_info')
+    session.pop('user_name')
     # return  render_template('/login.html')
     return redirect('/index')
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host='0.0.0.0')
