201070ee70ed870ebf9a0aa9c57a5eb8d964f8d2
linan <believelinan@aliyun.com>
2020-03-09 02:00:37 +0800

    database: enhance add

--- database/app.py
+++ database/app.py
@@ -8,7 +8,7 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
 
-class students(db.Model):
+class Students(db.Model):
     id = db.Column('student_id', db.Integer, primary_key = True)
     name = db.Column(db.String(100), unique=True)
     city = db.Column(db.String(50))
@@ -22,30 +22,33 @@ class students(db.Model):
 
 @app.route('/')
 def index():
-    return render_template('index.html', students = students.query.all() )
+    return render_template('index.html', students = Students.query.all() )
 
 @app.route('/add', methods = ['GET', 'POST'])
 def add():
     if request.method == 'POST':
-        if not request.form['name'] or not request.form['city'] or not request.form['addr']:
-            flash('Please enter all the fields', 'error')
+        id = request.form.get('id')
+        name = request.form.get('name')
+        city = request.form.get('city')
+        addr = request.form.get('addr')
+        pin = request.form.get('pin')
+        if not name or not city or not addr:
+            flash('Please enter all the required fields', 'error')
+            return render_template('add.html', id=id, name=name, city=city, addr=addr, pin=pin)
         else:
-            # 编辑
-            if request.form.get('id'):
-                id = request.form['id']
-                student = students.query.get(id)
-                student.name = request.form['name']
-                student.city = request.form['city']
-                student.addr = request.form['addr']
-                student.pin = request.form['pin']
-                # db.session.commit()
-                flash('Record was successfully updated')            
             # 添加
-            else:
-                student = students(request.form['name'], request.form['city'], request.form['addr'], request.form['pin'])
+            if id == '':
+                student = Students(name, city, addr, pin)
                 db.session.add(student)
-                # db.session.commit()
-                flash('Record was successfully added')
+                flash('Record was successfully added')            
+            # 编辑            
+            else:
+                student = Students.query.get(id)
+                student.name = name
+                student.city = city
+                student.addr = addr
+                student.pin = pin
+                flash('Record was successfully updated')            
             db.session.commit()
             return redirect(url_for('index'))
     return render_template('add.html')
@@ -53,18 +56,19 @@ def add():
 @app.route('/delete')
 def delete():
     id = request.args.get('id')
-    
     # 删除
-    student = students.query.get(id)
+    student = Students.query.get(id)
     db.session.delete(student)
     db.session.commit()
+    flash('Record was successfully deleted')
     return redirect(url_for('index'))
 
 @app.route('/edit')
 def edit():
     id = request.args.get('id')
-    student = students.query.get(id)
-    return render_template('add.html', student=student)
+    student = Students.query.get(id)
+    # return render_template('add.html', student=student)
+    return render_template('add.html', id=student.id, name=student.name, city=student.city, addr=student.addr, pin=student.pin)
 
 
 if __name__ == '__main__':
